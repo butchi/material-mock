8b9f85c9766b4715a147a67b23d7442b1fda4422
IWABUCHI Yu(u)ki <butchiyu+com-github@gmail.com>
2023-12-18 15:14:54 +0900

    モジュール追加

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import '@material/web/icon/icon.js';
+import '@material/web/iconbutton/icon-button.js';
 import '@material/web/button/filled-button.js';
 import '@material/web/button/outlined-button.js';
 import '@material/web/checkbox/checkbox.js';
+import '@material/web/checkbox/checkbox.js';
+import '@material/web/textfield/outlined-text-field.js';
